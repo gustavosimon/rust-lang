a1b6074593bc0cc3b949b2d2eacfe9eaa4492d36
gustavosimon <gustavo.andre.simon@hotmail.com>
2021-03-19 22:11:13 -0300

    adjust the string word

--- src/files.rs
+++ src/files.rs
@@ -2,13 +2,13 @@ use std::fs::File;
 use std::io::{Read, Result};
 
 pub fn run() {
-    match read_to_strig() {
+    match read_to_string() {
         Ok(_) => (),
         Err(_) => println!("Deu ruim"),
     }
 }
 
-fn read_to_strig() -> Result<()> {
+fn read_to_string() -> Result<()> {
     let mut file = File::open("./resources/file.txt")?;
     let mut data = String::new();
     file.read_to_string(&mut data)?;
